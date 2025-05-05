a22eb67f0c21cc75ba2cd4ebd7bb92846c0e69ba
Gilomyk <gilomyk@gmail.com>
2025-05-05 16:56:50 +0200

    add login page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
 import MainMenuAnonymous from './pages/MainMenuAnonymous.jsx';
 import MovieDetails from './pages/MovieDetails.jsx';
+import Login from './pages/Login.jsx';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Routes>
         <Route path="/" element={<MainMenuAnonymous />} />
         <Route path="/movie/:id" element={<MovieDetails />} />
+        <Route path="/login" element={<Login />} />
       </Routes>
     </Router>
   );
