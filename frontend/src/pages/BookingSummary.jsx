8cab90a01eb22d337294fdad95e16df6ef61b348
Gilomyk <gilomyk@gmail.com>
2025-06-01 16:29:26 +0200

    added CRSFToken in header for tickets request

--- frontend/src/pages/BookingSummary.jsx
+++ frontend/src/pages/BookingSummary.jsx
@@ -44,6 +44,12 @@ const BookingSummary = () => {
       .catch(err => console.error(err));
   }, [apiFetch]);
 
+  function getCookie(name) {
+    const value = `; ${document.cookie}`;
+    const parts = value.split(`; ${name}=`);
+    if (parts.length === 2) return parts.pop().split(';').shift();
+  }
+
 
   const handleConfirmBooking = async () => {
     setIsLoading(true);
@@ -67,6 +73,7 @@ const BookingSummary = () => {
           apiFetch(`${API_URL}/api/tickets/`, {
             method: "POST",
             headers: {
+              'X-CSRFToken': getCookie('csrftoken'),
               "Content-Type": "application/json",
             },
             body: JSON.stringify(ticket),
