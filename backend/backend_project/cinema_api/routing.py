5844596529fd1a32d935866b29c684991243dac3
jjaneq <bejgierjan@gmail.com>
2025-06-14 16:20:36 +0200

    Added websocket to listening new movies

--- backend/backend_project/cinema_api/routing.py
+++ backend/backend_project/cinema_api/routing.py
@@ -3,4 +3,5 @@ from . import consumers
 
 websocket_urlpatterns = [
     re_path(r'ws/movie_showings/(?P<show_id>\d+)/$', consumers.ShowConsumer.as_asgi()),
+    re_path(r'ws/movies/$', consumers.MovieConsumer.as_asgi()),
 ]
